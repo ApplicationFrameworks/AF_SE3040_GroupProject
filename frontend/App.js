127d4e75aac8209a0769243cdfb38f838f92b665
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-05-15 02:16:37 +0530

    Update Frontend

---
+++ frontend/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+            <button>Hi</button>  
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
