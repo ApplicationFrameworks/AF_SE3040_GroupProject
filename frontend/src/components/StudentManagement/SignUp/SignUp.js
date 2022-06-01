7447f554bac964fa9d281e557a913b6db53bc24e
PulasthiTharaka <86202408+PulasthiTharaka@users.noreply.github.com>
2022-06-01 23:22:42 +0530

    create signup page

--- frontend/src/components/StudentManagement/SignUp/SignUp.js
+++ frontend/src/components/StudentManagement/SignUp/SignUp.js
@@ -105,6 +105,7 @@ function SignUp() {
     }
 
     return (
+        <div>
             <div className="flex-container11">
                 <div  className="" align="center">
                     <div className="row">
@@ -112,10 +113,173 @@ function SignUp() {
                         </div>
                         <div className="col-11">
                             <div className="pb-2 px-5 d-flex align-items-center justify-content-between">
-                                <h2>Sign Up</h2>
+                                <h2 className="signup_text">Sign Up</h2>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+                <div className="flex-container12">
+                    <form onSubmit={register} className="boxSignUp">
+                        <div className="row">
+                            <div className="col-8">
+                                <div className="row">
+                                    <div className="col-md-6 mb-4">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type="text" id="firstname" placeholder="First Name"
+                                                required fullWidth
+                                                onChange={(event) => {
+                                                    setFirstName(event.target.value)
+                                                }}
+                                                inputProps={{style: {padding: 12}}}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-4">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type="text" id="lastname" placeholder="Last Name"
+                                                required fullWidth
+                                                onChange={(event) => {
+                                                    setLastName(event.target.value)
+                                                }}
+                                                inputProps={{style: {padding: 12}}}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4 mb-4">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type="text" id="it"
+                                                required fullWidth
+                                                onChange={(event) => {
+                                                    setITno(event.target.value)
+                                                }}
+                                                inputProps={{style: {padding: 12}}}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-8 mb-4">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type="email" id="email" placeholder="Email"
+                                                required fullWidth
+                                                onChange={(event) => {
+                                                    setEmail(event.target.value)
+                                                }}
+                                                inputProps={{style: {padding: 12}}}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-4">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type="tel" id="phone" placeholder="phone" required fullWidth
+                                                onChange={(event) => {
+                                                    setPhone(event.target.value)
+                                                }}
+                                                inputProps={{style: {padding: 12}, pattern: "[0-9]{10}"}}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type={showPassword ? "text" : "password"}
+                                                id="password" name="password" placeholder="Password" required fullWidth
+                                                onChange={(event) => {
+                                                    setPassword(event.target.value)
+                                                }}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton onClick={handleShowPassword}>
+                                                            {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                                inputProps={{style: {padding: 12}, pattern: "[A-Za-z0-9]{8,}"}}
+                                                onFocus={passwordOnFocus}
+                                                onBlur={passwordOnBlur}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <OutlinedInput
+                                                type={showPassword ? "text" : "password"}
+                                                id="confirmpassword" name="confirmpassword"
+                                                placeholder="Confirm Password" required fullWidth
+                                                onChange={(event) => {
+                                                    setConfirmPassword(event.target.value)
+                                                }}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton onClick={handleShowPassword}>
+                                                            {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                                inputProps={{style: {padding: 12}, pattern: "[A-Za-z0-9]{8,}"}}
+                                                onFocus={passwordOnFocus}
+                                                onBlur={passwordOnBlur}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-xl-12 mb-4">
+                                        {showMessage &&
+                                            <div className="PWmessage">
+                                                <p>Password must contain lowercase letters, uppercase letters, numbers
+                                                    and should consist minimum of 8 characters</p>
+                                            </div>
+                                        }
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                            <input id="terms" type="checkbox" required/>
+                                            <label htmlFor="terms">&nbsp;I agree to the <Link to="/terms">Terms and
+                                                Conditions</Link>.</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-4 d-flex justify-content-center">
+                                <div>
+                                    {previewSource ?
+                                        <img src={previewSource} alt="preview" className="previewImg"/>
+                                        :
+                                        <img src="/images/avatar.jpg" className="previewImg" alt="profile pic"/>
+                                    }
+                                    <div className="form-group">
+                                        <label htmlFor="profilepic">
+                                            <input
+                                                style={{display: 'none'}}
+                                                id="profilepic"
+                                                name="profilepic"
+                                                type="file"
+                                                onChange={handleFileInputChange}
+                                                value={fileInputState}
+                                            />
+
+                                            <Button color="primary" variant="contained" component="span">
+                                                <AddAPhotoIcon/> &nbsp; Upload Profile Picture
+                                            </Button>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="form-group">
+                                    <input className="form-submit-btn" type="submit" value="Sign Up"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <p>Already have an account? <Link to="/student/signin">Sign In</Link></p>
+                    </form>
                 </div>
 
 
