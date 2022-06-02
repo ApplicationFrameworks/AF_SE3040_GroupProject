0c5c1199655cd49d46088422642a2efc91899e49
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-06-03 01:57:20 +0530

    Removed test files

--- frontend/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("header renders with react testing tutorial in the document", ()=>{
-render(<App/>);
-const linkElement = screen.getByText(/This is react/i);
-expect(linkElement).toBeInTheDocument();
-});
