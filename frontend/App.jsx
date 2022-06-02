413e898509cee4b9d1fec917a7227cc63ed36d1b
Ishan Nadeera <shanf753@gmail.com>
2022-06-03 02:12:40 +0530

    Updated Frontend
    Updated Frontend for resolve conflicts

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -14,11 +14,11 @@ import Footer from './src/components/Footer/Footer';
 import AdminSignIn from './src/components/AdminManagement/AdminLogin';
 import StudentSignIn from './src/components/StudentManagement/SignIn/SignIn';
 import StudentSignUp from './src/components/StudentManagement/SignUp/SignUp';
-import AddRequest from "./src/components/RequestManagement/AddRequest";
-import DisplayRequest from "./src/components/RequestManagement/DisplayRequest";
-import AddTopics from "./src/components/TopicManagement/AddTopics/AddTopics";
-import ViewTopics from "./src/components/TopicManagement/ViewTopics/VIewTopics";
-import Modulepage from "./src/components/ModulePage/ModulePage";
+
+
+
+
+
 import VideoSubmission from "./src/components/VideoManagement/VideoSubmission/VideoSubmission";
 import MarkingScheme from "./src/components/MarkingSchemeManagement/MarkingSchemeSubmission/MarkingScheme";
 import ViewMarkingSchemes from "./src/components/MarkingSchemeManagement/ViewMarkingScheme/ViewMarkingSchemes";
@@ -36,7 +36,7 @@ function App() {
             <Header/>
             <Switch>
             <Route path="/" exact component={Homepage} />
-            <Route path="/module" exact component={Modulepage} />
+
             <Route path="/student/signin" exact component={StudentSignIn} />
             <Route path="/student/signup" exact component={StudentSignUp} />
             <Route path="/staff/register" exact component={StaffSignUp} />
@@ -45,14 +45,14 @@ function App() {
             <Route path="/staff/update/:id" exact component={StaffUpdate}/> 
             <Route path="/staff/all" exact component={DisplayStaff}/> 
             <Route path="/staff/all/:id" exact component={SingleStaff}/>
-            <Route path="/topic/add" exact component={AddTopics}/>
-            <Route path="/topic/view" exact component={ViewTopics}/>
+
+
             <Route path="/tdocAdd" exact component={DocumentSubmission}/>
             <Route path="/tdocView" exact component={ViewSubmissions}/> 
             <Route path="/tdocView/:id" exact component={OneSubmission}/> 
             <Route path="/admin/signin" exact component={AdminSignIn} />
-            <Route path="/student/request/:id" exact component={AddRequest}/>
-            <Route path="/request/:id" exact component={DisplayRequest}/>
+
+
             <Route path="/videoSubmissionAdd" exact component={VideoSubmission} />
             <Route path="/videoSubmissionView" exact component={ViewVideoSubmissions} />
             <Route path="/videoSubmissionView/:id" exact component={ViewOneVideoSubmission} />
